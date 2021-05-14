c01ad93ca70f89ead9d9162303f81afc62f9813d
Greg Foreman <gfore@mindspring.com>
2021-05-13 23:46:33 -0400

    rmv arrow functions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import React, { useState } from 'react';
 function App() {
   const [count, setCount] = useState(4);
 
+  function decrementCount() {
+    setCount(currentCount => currentCount - 1);
+  }
+
+  function incrementCount() {
+    setCount(currentCount => currentCount + 1);
+  }
+
   return (
     <div id="root">
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={decrementCount}>-</button>
       <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
